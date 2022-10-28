e77e428176f05155563c1c6b82eb7bee0542afb9
Giorgi Tchkoidze <mentorfps@gmail.com>
2022-10-28 23:27:25 +0400

    InProgress

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -13,7 +13,7 @@ def get_weth():
     # ABI
     # ADDRESS
     account = get_account()
-    weth = interfIWace.eth(config["networks"][network.show_active()]["weth_token"])
+    weth = interface.IWeth(config["networks"][network.show_active()]["weth_token"])
     tx = weth.deposit({"from": account, "value": 0.1 * 10**18})
     tx.wait(1)
     print(f"Received 0.1 WETH")
